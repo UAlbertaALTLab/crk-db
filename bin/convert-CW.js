92a309d4af03cbc3379cadce6aa33a27050db400
Daniel W. Hieber <dwhieb@gmail.com>
2021-10-25 23:18:22 -0600

    FIX: empty parentheses for MD entries (#91)

--- bin/convert-CW.js
+++ bin/convert-CW.js
@@ -4,8 +4,8 @@ import convert from '../lib/convert/CW.js';
 import program from 'commander';
 
 program
-.arguments(`<inputPath> <outputPath>`)
-.usage(`convert-cw <inputPath> <outputPath>`)
+.arguments(`<inputPath> [outputPath]`)
+.usage(`convert-cw <inputPath> [outputPath]`)
 .action(convert);
 
 program.parse(process.argv);
